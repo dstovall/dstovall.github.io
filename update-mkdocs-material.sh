7b540a503b3adf400d232d06c0d0f1befc4bbccc
Drew Stovall <drew.stovall@charterup.com>
2023-12-13 20:31:28 -0600

    Initial Material for MkDocs Site

---
+++ update-mkdocs-material.sh
@@ -0,0 +1,12 @@
+
+# Update pip
+pip install --upgrade pip
+
+# Update mkdocs
+pip install --upgrade mkdocs
+
+# Update mkdocs material
+pip install --upgrade mkdocs-material
+
+# Update mkdocs material plugin 'glightbox'
+pip install --upgrade mkdocs-glightbox
